7a232410f1acc34c9a66b8c42a2dd3853739dfd6
Shrijeet Paliwal <shrijeet@rocketfuel.com>
2011-10-15 19:18:42 -0700

    Use cloudera's cdh3u0 version

--- Library/Formula/hadoop.rb
+++ Library/Formula/hadoop.rb
@@ -1,37 +1,31 @@
 require 'formula'
 
 class Hadoop < Formula
-  url 'http://www.apache.org/dyn/closer.cgi?path=hadoop/core/hadoop-0.21.0/hadoop-0.21.0.tar.gz'
+  url 'http://archive.cloudera.com/cdh/3/hadoop-0.20.2-cdh3u0.tar.gz'
   homepage 'http://hadoop.apache.org/common/'
-  md5 'ec0f791f866f82a7f2c1319a54f4db97'
+  md5 'cc4b15e1dfd768109f98501a22105e95'
 
   def shim_script target
     <<-EOS.undent
     #!/bin/bash
-    exec #{libexec}/bin/#{target} $@
+    exec #{libexec}/bin/#{target} $*
     EOS
   end
 
   def install
     rm_f Dir["bin/*.bat"]
-    libexec.install %w[bin conf lib webapps mapred]
+    libexec.install %w[bin conf contrib lib webapps]
     libexec.install Dir['*.jar']
     bin.mkpath
     Dir["#{libexec}/bin/*"].each do |b|
       n = Pathname.new(b).basename
       (bin+n).write shim_script(n)
     end
-
-    inreplace "#{libexec}/conf/hadoop-env.sh",
-      "# export JAVA_HOME=/usr/lib/j2sdk1.6-sun",
-      "export JAVA_HOME=$(/usr/libexec/java_home)"
   end
 
-  def caveats; <<-EOS.undent
-    In Hadoop's config file:
-      #{libexec}/conf/hadoop-env.sh
-    $JAVA_HOME has been set to be the output of:
-      /usr/libexec/java_home
+  def caveats
+    <<-EOS.undent
+      $JAVA_HOME must be set for Hadoop commands to work.
     EOS
   end
 end
